c6ec430e0783127ef687e0926d810722b9e788b0
berr0461 <dus3694@naver.com>
2025-06-17 14:24:26 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const minus10 = () => {
+    setCount(count - 10);
+  } 
+
+  const minus1 = () => {
+    setCount(count - 1);
+  }
+
+  const plus1 = () => {
+    setCount(count + 1);
+  }
+
+  const plus10 = () => {
+    setCount(count + 10);
+  }
+  const reset = () => {
+    setCount(0);
+  }
+  return (
+    <>
+      
+      <div className='wrap'>
+        <h1>Simple Counter</h1>
+
+        <p className="number">{count}</p>
+
+        <div className="btn">
+          <button type='button' onClick={minus10}>-10</button>
+          <button type='button' onClick={minus1}>-1</button>
+          <button type='button' className='reset' onClick={reset}>RESET</button>
+          <button type='button' onClick={plus1}>+1</button>
+          <button type='button' onClick={plus10}>+10</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
